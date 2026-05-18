05e340c0ca278f5736bf44565e66c85bbca1911a
pjheeta <pjheeta@gmail.com>
2026-05-18 01:21:23 -0700

    Created basic framework

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
-import TextNode
+from textnode import TextNode, TextType
 def main():
-    print("hello world")
-    TextNode("This is some anchor text", "link", "https://www.boot.dev")
+    print (TextNode("This is some anchor text", TextType.BOLD, "https://www.boot.dev"))
+
+if __name__ == "__main__":
+    main()
