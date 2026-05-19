e82ab31458b00e61e7420eb1550761a3db202346
pjheeta <pjheeta@gmail.com>
2026-05-18 22:43:14 -0700

    Chapter 2 - Lessson 2 - Unit Tests

---
+++ src/test_textnode.py
@@ -0,0 +1,25 @@
+import unittest
+from textnode import TextNode, TextType
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_not_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a textrt node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_type(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.ITALIC)
+        self.assertNotEqual(node, node2)
+    
+    def test_url(self):
+        node = TextNode("This is a text node", TextType.BOLD,"www.google.com")
+        node2 = TextNode("This is a text node", TextType.BOLD, "www.hpe.com")
+        self.assertNotEqual(node, node2)
+if __name__ == "__main__":
+    unittest.main()
