c9dd4c330008099ffb6f9199679e2b02c5e49753
pjheeta <pjheeta@gmail.com>
2026-05-20 00:41:05 -0700

    Ch 2 - Lesson 4:LeafNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -11,7 +11,7 @@ class HtmlNode():
 
     def props_to_html(self):
         htmlString =""
-        if self.props == None:
+        if self.props is None:
             return ""
         for key, value in self.props.items():
             htmlString += " "+ key +'="' + value+'"'
@@ -20,4 +20,26 @@ class HtmlNode():
     def __repr__(self):
         return (f"HtmlNode({self.tag}, {self.value}, {self.children}, {self.props})")
     
+class LeafNode(HtmlNode):
+    def __init__(self, tag=None, value=None, props=None):
+        super().__init__(tag,value, None, props)
+
+    def to_html(self):
+        leafString =""
+        if self.value is None:
+            raise ValueError
+        if self.tag is None:
+            return self.value
+        else:
+            leafString +="<"+self.tag
+
+            if self.props is not None:
+                for key, value in self.props.items():
+                    leafString += " "+ key +'="' + value+ '"'
+
+            leafString += '>'+self.value + "</"+self.tag+">"
+        return leafString
+
+    def __repr__(self):
+        return (f"LeafNode({self.tag}, {self.value}, {self.props})")
 
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HtmlNode
+from htmlnode import HtmlNode, LeafNode
 
 class HtmlTextNode(unittest.TestCase):
     def test_props_to_html(self):
@@ -13,6 +13,18 @@ class HtmlTextNode(unittest.TestCase):
     def test_no_props_to_html(self):
         node = HtmlNode(props=None)
         self.assertEqual(node.props_to_html(), '')
+    
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_props(self):
+        node = LeafNode("a", "Hello, world!", {"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(), '<a href="https://www.google.com">Hello, world!</a>')
+
+    def test_leaf_broken(self):
+        node = LeafNode("a", "Hello, world!", {"href": "https://www.google.com"})
+        self.assertNotEqual(node.to_html(), '<a href="https://www.xhamster.com">Oh La La!!!!</a>')
 
 if __name__ == "__main__":
     unittest.main()
