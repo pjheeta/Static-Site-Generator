619b7fe180d2571d8275b88524d0a5bfc0e253ff
pjheeta <pjheeta@gmail.com>
2026-05-24 01:51:28 -0700

    Chapter 4 - Lesson 1: Split Blocks

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -2,6 +2,7 @@ import unittest
 from textnode import TextNode, TextType, text_node_to_html_node
 from inline import split_nodes_delimiter
 from regex import extract_markdown_images,extract_markdown_links, split_nodes_image,split_nodes_link
+from blocks import markdown_to_blocks
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -131,5 +132,26 @@ class TestTextNode(unittest.TestCase):
             ],
             new_nodes,
         )
+
+    def test_markdown_to_blocks(self):
+        md = """
+This is **bolded** paragraph
+
+This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line
+
+- This is a list
+- with items
+"""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "This is **bolded** paragraph",
+                "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
+                "- This is a list\n- with items",
+            ],
+        )
+
 if __name__ == "__main__":
     unittest.main()
