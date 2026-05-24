b237279e05e1f2ac2b10cf9e0988177f1c01712a
pjheeta <pjheeta@gmail.com>
2026-05-24 01:52:27 -0700

    Fixed Chapter 4 - Lesson 1: Split Blocks

--- blocks.py
+++ blocks.py
@@ -7,13 +7,3 @@ def markdown_to_blocks(markdown):
             temp = temp.strip()
             block.append (temp) 
     return block
-    
-
-text= """This is a heading
-
-"This is a paragraph of text. It has some **bold** and _italic_ words inside of it. 
-
-- This is the first list item in a list block
-- This is a list item
-- This is another list item"""
-print (markdown_to_blocks(text))
