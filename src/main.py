8fc543d1dba7f574de3e26d82cf53dbee6897650
pjheeta <pjheeta@gmail.com>
2026-05-24 18:22:51 -0700

    Chapter 4: Lesson 1 - Copy Static

--- src/main.py
+++ src/main.py
@@ -1,6 +1,27 @@
+import os
+import shutil
 from textnode import TextNode, TextType
+
+
 def main():
-    print (TextNode("This is some anchor text", TextType.BOLD, "https://www.boot.dev"))
+    src = "static"
+    dst = "public"
+    publicCleanup(src,dst)
+
+def publicCleanup(src,dst):
+
+    if os.path.exists(dst):        
+        shutil.rmtree(dst)
+    os.mkdir(dst)
+    files = os.listdir(src)
+    for file in files:
+        if os.path.isfile(os.path.join(src,file)) is True:
+            shutil.copy(os.path.join(src,file), os.path.join(dst,file)) 
+        else:
+            publicCleanup(os.path.join(src, file), os.path.join(dst, file))
+
+    
+
 
 if __name__ == "__main__":
     main()
