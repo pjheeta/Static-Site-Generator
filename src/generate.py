406354f0e39eedee8d68d24ecad47e7726c9a6f6
pjheeta <pjheeta@gmail.com>
2026-05-24 22:47:08 -0700

    Final Commit

--- src/generate.py
+++ src/generate.py
@@ -10,7 +10,7 @@ def extract_title(markdown):
             return (re.sub(r"^#\s*", "", temp))
     raise Exception("No h1 header found") 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print (f'Generating page from "{from_path}" to "{dest_path}" using "{template_path}"')
     markdown = open(from_path).read() 
     template = open(template_path).read() 
@@ -19,19 +19,23 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(markdown)
     template = template.replace("{{ Title }}",title)
     template = template.replace("{{ Content }}",markHtml)
+    ##  NEW FOR THE LAST ASSIGNMENT ##
+    template = template.replace('href="/', f'href="{basepath}')
+    template = template.replace('src="/', f'src="{basepath}')
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     open(dest_path, "w").write(template)
 
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+#def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path,basepath):    
     print (f'Generating page froms "{dir_path_content}" to "{dest_dir_path}" using "{template_path}"')
     dirContents = os.listdir(dir_path_content)
     print (dirContents)
     for item in dirContents:
         if os.path.isfile(os.path.join(dir_path_content,item)) is True:
             convertItem = item.replace(".md",".html")
-            generate_page(os.path.join(dir_path_content,item),template_path,os.path.join(dest_dir_path,convertItem))
+            generate_page(os.path.join(dir_path_content,item),template_path,os.path.join(dest_dir_path,convertItem), basepath)
         else:
-            generate_pages_recursive(os.path.join(dir_path_content,item),template_path,os.path.join(dest_dir_path,item))
+            generate_pages_recursive(os.path.join(dir_path_content,item),template_path,os.path.join(dest_dir_path,item), basepath)
 
 
--- src/main.py
+++ src/main.py
@@ -1,15 +1,21 @@
+import sys
 from textnode import TextNode, TextType
 from cleanup import publicCleanup
 from generate import generate_page, generate_pages_recursive
 
-
 def main():
     src = "static"
-    dst = "public"
+    dst = "docs"
+    
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+    else:
+        basepath = "/"
+    
     publicCleanup(src,dst)
     # generate_page("content/index.md", "template.html", "public/index.html")
-    generate_pages_recursive("content", "template.html", "public")
+    # generate_pages_recursive("content", "template.html", "public")
+    generate_pages_recursive("content", "template.html", "docs", basepath)
+
 if __name__ == "__main__":
     main()
-
-   
